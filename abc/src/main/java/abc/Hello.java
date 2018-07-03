8cc3137f51d5180c13dc8611bd7dbb1ae8845821
WindyGit <beizhibi100@gmail.com>
2018-07-03 02:28:58 -0400

    message

---
+++ abc/src/main/java/abc/Hello.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package abc;
+
+/**
+ * @author yuwin
+ *
+ */
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("abc");
+	}
+
+}
